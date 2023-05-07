c96b50a0e74a58353649c03385274eaaaf7440fc
nesrine ishak <nesrineissac@gmail.com>
2023-05-07 18:55:55 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require("express")
+const dotenv = require("dotenv")
+dotenv.config()
+require("./db")
+
+const app = express()
+const morgan = require("morgan")
+const bodyParser = require("body-parser")
+const cors = require("cors")
+
+app.use(cors())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(morgan('common'))
+
+app.listen(process.env.PORT, (err) => {
+
+    if (err) {
+        console.log(err)
+    } else {
+        console.log(`Listening to port ${process.env.PORT}`)
+    }
+
+})
+
+app.get('/', (req, res) => {
+
+    res.status(200).send("Server is responding")
+
+})
